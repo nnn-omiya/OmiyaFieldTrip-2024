81b407c7371121ffae902ed566d188e2f90101cd
kocchank <picolio912@gmail.com>
2024-05-13 02:48:46 +0900

    aaaa

---
+++ web/map/landmark.js
@@ -0,0 +1,104 @@
+const geojson = {
+    type: 'FeatureCollection',
+    features: [
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [139.6270861, 35.9074666]
+            },
+            properties: {
+                title: 'RaiBoC Hall',
+                description: '<p>うんち</p>'
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [139.630474, 35.9111529]
+            },
+            properties: {
+                title: '常盤珈琲焙煎所 大宮氷川参道店',
+                description: '<p>ちんちん</p>'
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [139.6301185, 35.9076921]
+            },
+            properties: {
+                title: 'パン屋',
+                description: '<p>ちんちん</p>'
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [139.6296188,35.9165063]
+            },
+            properties: {
+                title: '氷川神社',
+                description: '<p>ちんちん</p>'
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [139.6319459, 35.9185919]
+            },
+            properties: {
+                title: '大宮公園の小動物園',
+                description: '<p>ちんちん</p>'
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [139.6301794, 35.9170656]
+            },
+            properties: {
+                title: '大宮庭園',
+                description: '<p>ちんちん</p>'
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [139.629796, 35.9213188]
+            },
+            properties: {
+                title: '埼玉県立歴史と民俗の博物館',
+                description: '<p>ちんちん</p>'
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [139.6196083, 35.9052576]
+            },
+            properties: {
+                title: 'ソニックシティ',
+                description: '<p>ちんちん</p>'
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [139.6233728, 35.904454]
+            },
+            properties: {
+                title: 'アニメイト大宮',
+                description: '<p>ちんちん</p>'
+            }
+        }
+    ]
+};
