e446f0e0c32d14e5f99a19f794466199dec72593
kocchank <picolio912@gmail.com>
2024-05-13 15:50:34 +0900

    fix

--- js/common.js
+++ js/common.js
@@ -1,16 +1,17 @@
 //共通変数
-var currentTeamId = -1;
-var currentTeamName = "無し";
+let currentTeamId = -1;
+let currentTeamName = "無し";
 
+//GASのAPI
 const deployID = "AKfycbzuSjKsvnbsm6ruHkETqEqJ69c8p6VLWxBVvgsw1ZJd-TyfEVyVNzbtpsez-qz4AkBNkg"
 const base_url =  "https://script.google.com/a/macros/nnn.ed.jp/s/"+ deployID + "/exec";
 
 //ページ読み込まれたときに処理
 document.addEventListener("DOMContentLoaded", function() {
 	const localdata = localStorage.getItem('teamName');
-    if (JSON.parse(teamName)) {
-		currentTeamId = JSON.parse(teamName).id;
-        currentTeamName = JSON.parse(teamName).name;
+    if (JSON.parse(localdata)) {
+		currentTeamId = JSON.parse(localdata).id;
+        currentTeamName = JSON.parse(localdata).name;
 	} else if (!["/index.html","/login.html", "/about.html"].find(page => page == location.pathname)) {
 		console.log('teamNameは存在しません');
         location.replace("index.html");
