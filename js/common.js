7f28ad597dcf59cc3e5d0c8050919fce392ee625
chigayuki <107536212+chikinaN@users.noreply.github.com>
2024-05-17 04:00:16 +0900

    mapとかとか

--- js/common.js
+++ js/common.js
@@ -7,27 +7,24 @@ const deployID = "AKfycbwDly2n-GZgyVcnBz7cHA9ghtGzO3XOPAyH0ojfnrnelx_IUdqTRI1-xL
 const base_url =  "https://script.google.com/a/macros/nnn.ed.jp/s/"+ deployID + "/exec";
 
 //ページ読み込まれたときに処理
-document.addEventListener("DOMContentLoaded", function() {
-	const localdata = localStorage.getItem('teamName');
-    if (JSON.parse(localdata)) {
-		currentTeamId = JSON.parse(localdata).id;
-        currentTeamName = JSON.parse(localdata).name;
-	} else if (!["/index.html","/login.html", "/about.html"].find(page => page == location.pathname)) {
-		console.log('teamNameは存在しません');
-        location.replace("index.html");
-	}
 
-	if (document.cookie.indexOf("5=t") != -1) {
-		Array.from(document.querySelectorAll('*')).filter((b_)=>{
-			if (b_.textContent == '大宮の守り人') {b_.textContent = '大宮の花嫁';}
-		});
-	}
-});
+const localdata = localStorage.getItem('teamName');
+	if (JSON.parse(localdata)) {
+	currentTeamId = JSON.parse(localdata).id;
+			currentTeamName = JSON.parse(localdata).name;
+} else if (!["/index.html","/login.html", "/about.html"].find(page => page == location.pathname)) {
+	console.log('teamNameは存在しません');
+			location.replace("index.html");
+}
+
+if (document.cookie.indexOf("5=t") != -1) {
+	Array.from(document.querySelectorAll('*')).filter((b_)=>{
+		if (b_.textContent == '大宮の守り人') {b_.textContent = '大宮の花嫁';}
+	});
+}
+
 
 function getTeamName() {
     return currentTeamName
 }
-
-setInterval(function () {
-	console.log("おや、こんなところ見るなんてあなたプログラマー？\nスマートホームギルド、プログラマー募集してます\n@ちきな")
-}, 5000);
+console.log("おや、こんなところ見るなんてあなたプログラマー？\n徹夜で作ってるものだから穴があっても優しく使ってね\n私たちイノキャン大宮、プログラマー募集してます\n@ちきなにDMください")
